ebb9f88e9c0aaf24dfb2dde015a07d0e89f58783
rowlavel <rowlavel@iu.edu>
2021-05-06 21:10:37 -0400

    min d=2

--- frac_change_forecasting/hmms/gaussian_hmm_grid_search.py
+++ frac_change_forecasting/hmms/gaussian_hmm_grid_search.py
@@ -91,7 +91,7 @@ class GHMM(Model):
 
 if __name__ == "__main__":
     components = [2,3,4,5,6,7,8,9]
-    latency = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+    latency = [2,3,4,5,6,7,8,9,10,11,12,13,14,15]
 
     best = {'params': None, 'aamape':math.inf}
 
