d4705568de67df32fa94eefc5394e2cefd64e922
rowlavel <rowlavel@iu.edu>
2021-05-17 15:25:44 -0400

    testig time

--- frac_change_forecasting/regressors/random_forest.py
+++ frac_change_forecasting/regressors/random_forest.py
@@ -1,6 +1,8 @@
 from sklearn.ensemble import RandomForestRegressor
 from model import *
 from test import *
+import math
+import time
 
 class RandomForest(Model):
     def __init__(self, params):
@@ -27,11 +29,18 @@ if __name__ == '__main__':
         'name':'RandomForestRegressor'
     }
 
-    test = Test(Model=RandomForest, params=params, tests=paper_tests, f='rf-paper-tests.json', plot=True)
-    test.fixed_origin_tests(folder='random_forest')
+    # test = Test(Model=RandomForest, params=params, tests=paper_tests, f='rf-paper-tests.json', plot=True)
+    # test.fixed_origin_tests(folder='random_forest')
 
-    test = Test(Model=RandomForest, params=params, tests=own_tests, f='rf-own-tests.json', plot=True)
-    test.fixed_origin_tests(folder='random_forest')
+    # test = Test(Model=RandomForest, params=params, tests=own_tests, f='rf-own-tests.json', plot=True)
+    # test.fixed_origin_tests(folder='random_forest')
 
-    test = Test(Model=RandomForest, params=params, tests=rolling_window_tests, f='rf-rolling-tests.json', plot=True)
-    test.rolling_window_test(folder='random_forest')
+    start = time.time()
+    test = Test(Model=RandomForest, params=params, tests=rolling_window_tests, f='rf-rolling-tests.json', plot=False)
+    results = test.rolling_window_test(folder='random_forest')
+    end = time.time()
+    print(f'time elapsed: {end-start/60}')
+    print(f'average R2 score: {np.average(results["R2"].values())}')
+    print(f'average MAPE: {np.average(results["MAPE"].values())}')
+
+
